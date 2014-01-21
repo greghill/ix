848a31e0466feaebfeb3431ae8eb5d87686252dd
Adam Belay <abelay@stanford.edu>
2014-01-21 14:18:44 -0800

    initial pass on atomics 	- this time memory barriers 	- still need atomic add, sub, etc.

---
+++ ix/inc/ix/atomic.h
@@ -0,0 +1,30 @@
+/*
+ * atomic.h - utilities for atomically manipulating memory
+ */
+
+/**
+ * mb - a memory barrier
+ *
+ * Ensures all loads and stores before the barrier complete
+ * before all loads and stores after the barrier.
+ */
+#define mb() _mm_mfence()
+
+/**
+ * rmb - a read memory barrier
+ *
+ * Ensures all loads before the barrier complete before
+ * all loads after the barrier.
+ */
+#define rmb() _mm_lfence()
+
+/**
+ * wmb - a write memory barrier
+ *
+ * Ensures all stores before the barrier complete before
+ * all stores after the barrier.
+ */
+#define wmb() _mm_sfence()
+
+#include <emmintrin.h>
+
