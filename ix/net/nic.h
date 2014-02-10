68c42067778de67cb960db1aa59095bffb709969
George Prekas <prekgeo@yahoo.com>
2014-02-10 18:06:13 +0100

    add abstract nic interface

---
+++ ix/net/nic.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <rte_config.h>
+#include <rte_mbuf.h>
+
+struct nic_operations {
+	int (*has_pending_pkts) (void);
+	int (*receive_one_pkt) (struct rte_mbuf **pkt);
+	int (*tx_one_pkt) (struct rte_mbuf *pkt);
+	struct rte_mbuf * (*alloc_pkt) (void);
+	void (*free_pkt) (struct rte_mbuf *pkt);
+};
+
+extern struct nic_operations *nic_ops;
