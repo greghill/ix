54e7ee7db9b6742adeca861594593ef9691d1f49
George Prekas <prekgeo@yahoo.com>
2014-02-17 15:12:00 +0100

    select nic implementation from command line options

--- ix/net/main.c
+++ ix/net/main.c
@@ -1,4 +1,5 @@
 #include <getopt.h>
+#include <string.h>
 
 #include <asm/cpu.h>
 #include <ix/dyncore.h>
@@ -14,8 +15,10 @@ extern int arp_init(void);
 extern int dpdk_init(int argc, char *argv[]);
 
 extern struct nic_operations dpdk_nic_ops;
+extern struct nic_operations pping_nic_ops;
+extern struct nic_operations virtual_nic_ops;
 
-struct nic_operations *nic_ops = &dpdk_nic_ops;
+struct nic_operations *nic_ops;
 
 static int receive_pkts(__attribute__((unused)) void *dummy)
 {
@@ -43,10 +46,11 @@ void parse_arguments(int argc, char *argv[])
 	static struct option long_options[] = {
 		{"ip", required_argument, 0, 'i'},
 		{"mac", required_argument, 0, 'm'},
+		{"nic", required_argument, 0, 'n'},
 		{NULL, 0, NULL, 0}
 	};
 
-	while ((c = getopt_long(argc, argv, "i:m:", long_options, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "i:m:n:", long_options, NULL)) != -1) {
 		switch (c) {
 		case 'i':
 		{
@@ -64,6 +68,18 @@ void parse_arguments(int argc, char *argv[])
 				exit(1);
 			}
 			break;
+		case 'n':
+			if (!strcmp(optarg, "dpdk")) {
+				nic_ops = &dpdk_nic_ops;
+			} else if (!strcmp(optarg, "pping")) {
+				nic_ops = &pping_nic_ops;
+			} else if (!strcmp(optarg, "virtual")) {
+				nic_ops = &virtual_nic_ops;
+			} else {
+				fprintf(stderr, "Invalid NIC specified.\n");
+				exit(1);
+			}
+			break;
 		default:
 			exit(1);
 		}
@@ -75,12 +91,17 @@ int main(int argc, char *argv[])
 	static char *dpdk_argv[] = {"", "-c1", "-n1"};
 
 	parse_arguments(argc, argv);
+	if (!nic_ops) {
+		fprintf(stderr, "Error: NIC not specified. Aborting.\n");
+		exit(1);
+	}
 
 	timer_init();
 	arp_init();
-
-	optind = 1;
-	dpdk_init(3, dpdk_argv);
+	if (nic_ops == &dpdk_nic_ops) {
+		optind = 1;
+		dpdk_init(3, dpdk_argv);
+	}
 	nic_ops->init();
 	dyncore_init(receive_pkts, has_pending_pkts);
 
