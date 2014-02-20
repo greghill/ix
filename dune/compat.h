ded7bed14a8671cf6548dcf6c1260c3b75b30253
Adam Belay <abelay@stanford.edu>
2014-02-19 18:08:28 -0800

    dune: initial import of kernel driver

---
+++ dune/compat.h
@@ -0,0 +1,29 @@
+#ifndef __DUNE_COMPAT_H_
+#define __DUNE_COMPAT_H_
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,4,0)
+#include <asm/fpu-internal.h>
+#endif
+
+#if !defined(VMX_EPT_AD_BIT)
+#define VMX_EPT_AD_BIT          (1ull << 21)
+#define VMX_EPT_AD_ENABLE_BIT   (1ull << 6)
+#endif
+
+#ifndef VMX_EPT_EXTENT_INDIVIDUAL_BIT
+#define VMX_EPT_EXTENT_INDIVIDUAL_BIT           (1ull << 24)
+#endif
+
+#ifndef X86_CR4_PCIDE
+#define X86_CR4_PCIDE		0x00020000 /* enable PCID support */
+#endif
+
+#ifndef SECONDARY_EXEC_ENABLE_INVPCID
+#define SECONDARY_EXEC_ENABLE_INVPCID	0x00001000
+#endif
+
+#ifndef X86_CR4_FSGSBASE
+#define X86_CR4_FSGSBASE	X86_CR4_RDWRGSFS
+#endif
+
+#endif /* __DUNE_COMPAT_H_ */
