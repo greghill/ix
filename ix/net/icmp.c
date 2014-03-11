798b59d710d1049d5ba69dc4af2fc62dd7eafbed
George Prekas <prekgeo@yahoo.com>
2014-03-11 16:42:00 +0100

    implement icmp echo request function

--- ix/net/icmp.c
+++ ix/net/icmp.c
@@ -75,3 +75,63 @@ out:
 	mbuf_free(pkt);
 }
 
+int icmp_echo(struct ip_addr *dest, uint16_t id, uint16_t seq, uint64_t timestamp)
+{
+	int ret;
+	struct mbuf *pkt;
+	struct eth_hdr *ethhdr;
+	struct ip_hdr *iphdr;
+	struct icmp_pkt *icmppkt;
+	uint64_t *icmptimestamp;
+	uint16_t len;
+
+	pkt = mbuf_alloc_local();
+	if (unlikely(!pkt))
+		return -ENOMEM;
+
+	ethhdr = mbuf_mtod(pkt, struct eth_hdr *);
+	iphdr = mbuf_nextd(ethhdr, struct ip_hdr *);
+	icmppkt = mbuf_nextd(iphdr, struct icmp_pkt *);
+	icmptimestamp = mbuf_nextd_off(icmppkt, uint64_t *, sizeof(struct icmp_hdr) + 4);
+
+	if (arp_lookup_mac(dest, &ethhdr->dhost)) {
+		log_err("ARP lookup failed.\n");
+		mbuf_free(pkt);
+		return -EIO;
+	}
+
+	ethhdr->shost = cfg_mac;
+	ethhdr->type = hton16(ETHTYPE_IP);
+
+	len = sizeof(struct icmp_hdr) + 4 + sizeof(uint64_t);
+
+	iphdr->header_len = sizeof(struct ip_hdr) / 4;
+	iphdr->version = 4;
+	iphdr->tos = 0;
+	iphdr->len = hton16(sizeof(struct ip_hdr) + len);
+	iphdr->id = 0;
+	iphdr->off = 0;
+	iphdr->ttl = 64;
+	iphdr->chksum = 0;
+	iphdr->proto = IPPROTO_ICMP;
+	iphdr->src_addr.addr = hton32(cfg_host_addr.addr);
+	iphdr->dst_addr.addr = hton32(dest->addr);
+	iphdr->chksum = chksum_internet((void *) iphdr, sizeof(struct ip_hdr));
+
+	icmppkt->hdr.type = ICMP_ECHO;
+	icmppkt->hdr.code = ICMP_ECHOREPLY;
+	icmppkt->hdr.chksum = 0;
+	icmppkt->icmp_id = hton16(id);
+	icmppkt->icmp_seq = hton16(seq);
+	*icmptimestamp = timestamp;
+	icmppkt->hdr.chksum = chksum_internet((void *) icmppkt, len);
+
+	ret = eth_tx_xmit_one(eth_tx, pkt, sizeof(struct eth_hdr) + sizeof(struct ip_hdr) + len);
+
+	if (unlikely(ret != 1)) {
+		mbuf_free(pkt);
+		return -EIO;
+	}
+
+	return 0;
+}
