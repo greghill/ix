18996ce7cd83ac0ecd8b9348b4a6808d1d96e3d4
George Prekas <prekgeo@yahoo.com>
2014-03-26 19:45:00 +0100

    implement TCP timers

--- ix/lwip/misc.c
+++ ix/lwip/misc.c
@@ -1,4 +1,5 @@
 #include <ix/mbuf.h>
+#include <ix/timer.h>
 
 #include <net/ip.h>
 
@@ -19,9 +20,38 @@ void tcp_input(struct pbuf *p, struct netif *inp);
 
 struct ip_globals ip_data;
 
-/* TODO: implement */
+extern void tcp_tmr(void);
+extern struct tcp_pcb *tcp_active_pcbs;
+extern struct tcp_pcb *tcp_tw_pcbs;
+/* in us */
+#define TCP_TMR_INTERVAL 250000
+
+static struct timer tcp_timer;
+static int tcpip_tcp_timer_active;
+
+static void tcpip_tcp_timer(struct timer *t)
+{
+	/* call TCP timer handler */
+	tcp_tmr();
+	/* timer still needed? */
+	if (tcp_active_pcbs || tcp_tw_pcbs) {
+		/* restart timer */
+		timer_add(&tcp_timer, TCP_TMR_INTERVAL);
+	} else {
+		/* disable timer */
+		tcpip_tcp_timer_active = 0;
+	}
+}
+
 void tcp_timer_needed(void)
 {
+	/* timer is off but needed again? */
+	if (!tcpip_tcp_timer_active && (tcp_active_pcbs || tcp_tw_pcbs)) {
+		/* enable and start timer */
+		tcpip_tcp_timer_active = 1;
+		timer_init_entry(&tcp_timer, &tcpip_tcp_timer);
+		timer_add(&tcp_timer, TCP_TMR_INTERVAL);
+	}
 }
 
 struct netif *ip_route(struct ip_addr *dest)
