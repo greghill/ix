greghill/ix

commit 4b3632b0ca4fc89fe918dcfd5892c54bb64cf9f1

configure lwip to use malloc/free instead of its own infrastructure

ix/inc/lwip/lwipopts.h
@@ -4,6 +4,8 @@
 
 #define MEMP_NUM_TCP_PCB 65535
 #define MEMP_NUM_TCP_SEG 65535
+#define MEM_LIBC_MALLOC 1
+#define MEMP_MEM_MALLOC 1
 
 #define	LWIP_DEBUG		LWIP_DBG_OFF
 #define	TCP_CWND_DEBUG		LWIP_DBG_OFF

ix/net/tcp_out.c
@@ -789,7 +789,6 @@ tcp_enqueue_flags(struct tcp_pcb *pcb, u8_t flags)
     TCP_STATS_INC(tcp.memerr);
     return ERR_MEM;
   }
-  LWIP_ASSERT("seg->tcphdr not aligned", ((mem_ptr_t)seg->tcphdr % 4) == 0);
   LWIP_ASSERT("tcp_enqueue_flags: invalid segment length", seg->len == 0);
 
   LWIP_DEBUGF(TCP_OUTPUT_DEBUG | LWIP_DBG_TRACE,

commit dcdbe7ac140bcd0d038714558089eb668872cea9

remove unused #defines caused by commit 4b3632b

ix/inc/lwip/lwipopts.h
@@ -2,8 +2,6 @@
 #define	LWIP_TCP	1
 #define	NO_SYS		1
 
-#define MEMP_NUM_TCP_PCB 65535
-#define MEMP_NUM_TCP_SEG 65535
 #define MEM_LIBC_MALLOC 1
 #define MEMP_MEM_MALLOC 1
 