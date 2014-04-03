cadcbe882179a44b95e1fc76af5c7122020da304
George Prekas <prekgeo@yahoo.com>
2014-04-03 16:52:51 +0200

    enable multicore support

--- ix/core/init.c
+++ ix/core/init.c
@@ -2,6 +2,8 @@
  * init.c - main system and CPU core initialization
  */
 
+#include <pthread.h>
+
 #include <ix/stddef.h>
 #include <ix/log.h>
 #include <ix/errno.h>
@@ -28,6 +30,11 @@
 #define MAX_QUEUES 16
 #define MAX_QUEUES_PER_CORE 16
 
+struct cpu_thread_params {
+	int cpu;
+	int nb_rx_queues;
+};
+
 extern int net_init(void);
 extern int ixgbe_init(struct pci_dev *pci_dev, struct rte_eth_dev **ethp);
 extern int virtual_init(void);
@@ -36,6 +43,8 @@ extern int sandbox_init(int argc, char *argv[]);
 extern void tcp_init(void);
 
 volatile int uaccess_fault;
+
+static pthread_barrier_t barrier;
 
 static int hw_init_one(const char *pci_addr)
 {
@@ -243,18 +252,83 @@ static int cpu_networking_init(int nb_rx_queues)
 	return 0;
 }
 
+void *start_cpu(void *arg)
+{
+	int ret;
+	struct cpu_thread_params *params;
+
+	pthread_barrier_wait(&barrier);
+
+	params = arg;
+
+	ret = init_this_cpu(params->cpu);
+	if (ret) {
+		log_err("init: failed to initialize CPU %d\n", params->cpu);
+		exit(ret);
+	}
+
+	ret = cpu_networking_init(params->nb_rx_queues);
+	if (ret) {
+		log_err("init: failed to initialize networking for cpu\n");
+		exit(ret);
+	}
+
+	free(params);
+
+	main_loop();
+
+	return NULL;
+}
+
+int parse_cpu_list(char *list, int *cpu, int max_cpus, int *cpu_list_count)
+{
+	int i;
+	int val;
+	char *tok;
+
+	*cpu_list_count = 0;
+	tok = strtok(list, ",");
+	while (tok) {
+		if (*cpu_list_count >= max_cpus) {
+			log_err("init: a maximum number of %d cpus are supported.\n", max_cpus);
+			return 1;
+		}
+		val = atoi(tok);
+		if (val < 0 || val >= cpu_count) {
+			log_err("init: invalid cpu specified '%s'\n", tok);
+			return 1;
+		}
+		for (i = 0; i < *cpu_list_count; i++) {
+			if (cpu[i] == val) {
+				log_err("init: cpu specified multiple times '%d'\n", val);
+				return 1;
+			}
+		}
+		cpu[*cpu_list_count] = val;
+		(*cpu_list_count)++;
+		tok = strtok(NULL, ",");
+	}
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int ret;
 #ifdef ENABLE_PCAP
 	int pcap_read_mode = 0;
 #endif
+	pthread_t thread;
+	int cpu_list_count;
+	int cpu[MAX_QUEUES];
+	int i;
+	struct cpu_thread_params *cpu_thread_params;
 
 	log_info("init: starting IX\n");
 
 	if (argc < 2) {
 		log_err("init: invalid arguments\n");
-		log_err("init: format -> ix [ETH_PCI_ADDR]\n");
+		log_err("init: format -> ix ETH_PCI_ADDR [CPU,...]\n");
 		return -EINVAL;
 	}
 
@@ -262,6 +336,15 @@ int main(int argc, char *argv[])
 	if (ret) {
 		log_err("init: failed to initalize CPU cores\n");
 		return ret;
+	}
+
+	if (argc >= 3) {
+		ret = parse_cpu_list(argv[2], cpu, sizeof(cpu) / sizeof(cpu[0]), &cpu_list_count);
+		if (ret)
+			return ret;
+	} else {
+		cpu[0] = 1;
+		cpu_list_count = 1;
 	}
 
 	ret = dune_init(false);
@@ -278,7 +361,26 @@ int main(int argc, char *argv[])
 		return ret;
 	}
 
-	ret = init_this_cpu(1);
+	ret = pthread_barrier_init(&barrier, NULL, cpu_list_count);
+	if (ret) {
+		log_err("init: failed to init barrier\n");
+		return ret;
+	}
+
+	for (i = 1; i < cpu_list_count; i++) {
+		cpu_thread_params = malloc(sizeof(*cpu_thread_params));
+		cpu_thread_params->cpu = cpu[i];
+		cpu_thread_params->nb_rx_queues = MAX_QUEUES / cpu_list_count;
+		if (i >= cpu_list_count - MAX_QUEUES % cpu_list_count)
+			cpu_thread_params->nb_rx_queues++;
+		ret = pthread_create(&thread, NULL, start_cpu, cpu_thread_params);
+		if (ret) {
+			log_err("init: failed to spawn thread\n");
+			return ret;
+		}
+	}
+
+	ret = init_this_cpu(cpu[0]);
 	if (ret) {
 		log_err("init: failed to initialize the local CPU\n");
 		return ret;
@@ -318,6 +420,8 @@ int main(int argc, char *argv[])
 			log_err("init: failed to initialize ethernet device\n");
 			return ret;
 		}
+
+		pthread_barrier_wait(&barrier);
 	}
 
 	ret = net_init();
@@ -326,7 +430,7 @@ int main(int argc, char *argv[])
 		return ret;
 	}
 
-	ret = cpu_networking_init(MAX_QUEUES);
+	ret = cpu_networking_init(MAX_QUEUES / cpu_list_count);
 	if (ret) {
 		log_err("init: failed to initialize networking for cpu\n");
 		return ret;
