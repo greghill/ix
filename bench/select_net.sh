4eaedfe2b384d83c2fe3a0c99d66a7c35e427891
George Prekas <prekgeo@yahoo.com>
2014-04-10 20:38:30 +0200

    bench: enhance select_net.sh tool and add README entry

--- bench/select_net.sh
+++ bench/select_net.sh
@@ -2,72 +2,70 @@
 
 set -e
 
-BMOS_PATH=~/epfl1/prg/me/bmos
+# set default values if not found in the environment
+foo=${BMOS_PATH:=~/epfl1/prg/me/bmos}
+foo=${ALL_IFS:="eth4 eth5 eth6 eth7 eth8 eth9 eth10 eth11 bond0"}
+foo=${BOND:=bond0}
+foo=${BOND_SLAVES:="eth4 eth5 eth6 eth7"}
+foo=${SINGLE_NIC:=eth11}
+foo=${IP:=192.168.21.1}
+foo=${PAGE_COUNT_2MB:=10000}
 
-BOND1=eth4
-BOND2=eth5
-BOND3=eth6
-BOND4=eth7
-REST1=eth8
-REST2=eth9
-REST3=eth10
-NIC=eth11
-
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
-
-common() {
-  service irqbalance stop || true
-  ifdown $BOND1 $BOND2 $BOND3 $BOND4
-  ifdown bond0
+tear_down() {
+  service irqbalance stop >/dev/null 2>&1 || true
+  ifdown $ALL_IFS 2>/dev/null
   modprobe -r ixgbe
-  rmmod dune || true
-  rmmod igb_stub || true
-  ifdown $BOND1 $BOND2 $BOND3 $BOND4 $NIC $REST1 $REST2 $REST3
-  ifdown bond0
-  echo 0 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-  echo 0 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+  rmmod dune 2>/dev/null || true
+  rmmod igb_stub 2>/dev/null || true
+  for i in /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages; do
+    echo 0 > $i
+  done
 }
 
-common_linux() {
-  common
-  service irqbalance start
+setup_linux() {
+  tear_down
+  service irqbalance start >/dev/null
   modprobe ixgbe
-  ifdown $BOND2 $BOND3 $BOND4 $REST1 $REST2 $REST3 $NIC
-  ifdown bond0
+  ifdown $ALL_IFS 2>/dev/null
 }
 
-common_ix() {
-  common
-  (cd $BMOS_PATH/dune && make)
-  (cd $BMOS_PATH/igb_stub && make)
+setup_ix() {
+  tear_down
+  (cd $BMOS_PATH/dune && make -j64 >/dev/null 2>&1)
+  (cd $BMOS_PATH/igb_stub && make -j64 >/dev/null)
   insmod $BMOS_PATH/dune/dune.ko
   insmod $BMOS_PATH/igb_stub/igb_stub.ko
+}
+
+invalid_params() {
+  echo 'invalid parameters' >&2
+  exit 1
 }
 
 if [ -z $1 ]; then
   echo 'missing parameter' >&2
   exit 1
 elif [ $1 = 'none' ]; then
-  common
-elif [ $1 = 'bond' ]; then
-  common_linux
-  ifup $BOND1 $BOND2 $BOND3 $BOND4
-  ifup bond0
-elif [ $1 = 'single' ]; then
-  common_linux
-  ifconfig $NIC 192.168.21.1
-elif [ $1 = 'single-onecore' ]; then
-  common_linux
-  ifconfig $NIC 192.168.21.1
-elif [ $1 = 'ix-numa0' ]; then
-  common_ix
-  echo 10000 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-elif [ $1 = 'ix-numa1' ]; then
-  common_ix
-  echo 10000 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+  tear_down
+elif [ $1 = 'linux' -a "$2" = 'single' ]; then
+  setup_linux
+  ifconfig $SINGLE_NIC $IP
+elif [ $1 = 'linux' -a "$2" = 'bond' ]; then
+  setup_linux
+  ifup $BOND_SLAVES >/dev/null
+elif [ $1 = 'ix' ]; then
+  SYSFILE=/sys/devices/system/node/$2/hugepages/hugepages-2048kB/nr_hugepages
+  if [ -f $SYSFILE ]; then
+    setup_ix
+    echo $PAGE_COUNT_2MB > $SYSFILE
+  else
+    invalid_params
+  fi
+elif [ $1 = '--help' ]; then
+  echo "Usage:"
+  echo "  $0 none"
+  echo "  $0 linux [single|bond]"
+  echo "  $0 ix    [node0|node1|...]"
 else
-  echo 'invalid parameter' >&2
-  exit 1
+  invalid_params
 fi
