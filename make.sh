ace974ed454c09eb9dca3252a0df91fa72c96a73
George Prekas <prekgeo@yahoo.com>
2014-04-19 12:39:14 +0200

    update make.sh

--- make.sh
+++ make.sh
@@ -3,7 +3,11 @@
 set -e
 
 DIR=`dirname $0`
-(cd $DIR/ix && make -j64>/dev/null)
-(cd $DIR/libix && make -j64>/dev/null)
-(cd $DIR/apps/tcp && make -j64>/dev/null)
-(cd $DIR/bench && make -j64>/dev/null)
+TARGET=$1
+PARAMS="-j64 -s"
+make -C $DIR/bench    $PARAMS $TARGET
+make -C $DIR/dune     $PARAMS $TARGET
+make -C $DIR/igb_stub $PARAMS $TARGET
+make -C $DIR/ix       $PARAMS $TARGET
+make -C $DIR/libix    $PARAMS $TARGET
+make -C $DIR/apps/tcp $PARAMS $TARGET
