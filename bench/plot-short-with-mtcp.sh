202e5757830d3276611129ae1b4d0b9cce3be370
George Prekas <prekgeo@yahoo.com>
2014-06-04 15:12:06 +0200

    bench: add copy_files into plot-short-with-mtcp.sh

--- bench/plot-short-with-mtcp.sh
+++ bench/plot-short-with-mtcp.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 nth() {
-  ls stanford_fig4_results/*/short/$2/data|sort -r|sed -n "$1{p;q;}"
+  ls results/*/short/$2/data|sort -r|sed -n "$1{p;q;}"
+}
+
+copy_files() {
+  DEST=$1
+  mkdir -p $DEST
+  shift
+  while [ $# -gt 0 ]; do
+    FILE=$1
+    tar -c `dirname $FILE` | tar --strip-components=2 -C $DEST -x
+    shift
+  done
 }
 
 FORMAT=${1:-eps}
@@ -23,6 +34,7 @@ echo $SHORT_FILES|tr ' ' '\n'
 
 if [ $FORMAT = 'eps' ]; then
   OUTDIR=../papers/osdi14/figs
+  copy_files ../papers/osdi14/figs/data $SHORT_FILES
 else
   OUTDIR=figures
 fi
