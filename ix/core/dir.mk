4589546a8d81016a6a94c1618eaf02ac2c42011b
Adam Belay <abelay@stanford.edu>
2014-07-24 14:05:17 -0700

    remove some more dead code

--- ix/core/dir.mk
+++ ix/core/dir.mk
@@ -1,6 +1,6 @@
 # Makefile for the core system
 
-SRC = ethdev.c ethqueue.c cfg.c control_plane.c cpu.c init.c log.c mbuf.c mem.c mempool.c page.c pci.c queue.c syscall.c toeplitz.c tcpecho.c timer.c vm.c
+SRC = ethdev.c ethqueue.c cfg.c control_plane.c cpu.c init.c log.c mbuf.c mem.c mempool.c page.c pci.c queue.c syscall.c toeplitz.c timer.c vm.c
 
 ifneq ($(ENABLE_KSTATS),)
 SRC += kstats.c tailqueue.c
