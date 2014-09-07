53de01808dbc2a68a7a0e493dada9c0f3105c8cb
George Prekas <prekgeo@yahoo.com>
2014-09-08 00:39:44 +0200

    bench: more machines for memcached benchmark

--- experiments/epfl/osdi14_basic_agentlist.sh
+++ experiments/epfl/osdi14_basic_agentlist.sh
@@ -16,7 +16,13 @@ echo icnals10
 echo icnals11
 echo icnals12
 echo icnals13
+echo icnals14
 echo icnals15
 echo icnals16
 echo icnals17
-echo icnals18
+echo icnals19
+echo icnals20
+echo icnalsp3s1
+echo icnalsp3s2
+echo icnalsp3s3
+echo icnalsp3s4
