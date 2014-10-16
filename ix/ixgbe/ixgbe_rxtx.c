7e1d2224af66f558c5fe0cff4dd25ab95c2825d2
George Prekas <prekgeo@yahoo.com>
2014-10-16 17:53:49 +0200

    Revert "temporarily replace flow group identifier with queue identifier to avoid performance regression in connection scalability benchmark for the 4x10GbE environment"
    This reverts commit a8223fe1f81bcded42c146a007e987c1d4aaec2c.

--- ix/ixgbe/ixgbe_rxtx.c
+++ ix/ixgbe/ixgbe_rxtx.c
@@ -169,7 +169,8 @@ static int ixgbe_rx_poll(struct eth_rx_queue *rx)
 
 		b = rxqe->mbuf;
 		b->len = le32_to_cpu(rxd.wb.upper.length);
-		b->fg_id = rxq->queue_id;
+		b->fg_id = (le32_to_cpu(rxd.wb.lower.hi_dword.rss) &
+				        (ETH_RSS_RETA_NUM_ENTRIES - 1));
 
 		new_b = mbuf_alloc_local();
 		if (unlikely(!new_b)) {
