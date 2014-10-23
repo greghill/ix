04eaba12fe0c6c8a4d413042f3a93fbf4e4d77dd
George Prekas <prekgeo@yahoo.com>
2014-10-23 20:17:12 +0200

    add percpu timer implementation. enabled only by ENABLE_KSTATS

--- ix/core/dir.mk
+++ ix/core/dir.mk
@@ -3,7 +3,7 @@
 SRC = ethdev.c ethfg.c ethqueue.c cfg.c control_plane.c cpu.c init.c log.c mbuf.c mem.c mempool.c page.c pci.c queue.c syscall.c toeplitz.c timer.c vm.c
 
 ifneq ($(ENABLE_KSTATS),)
-SRC += kstats.c tailqueue.c
+SRC += kstats.c tailqueue.c timer_percpu.c
 endif
 
 $(eval $(call register_dir, core, $(SRC)))
--- ix/inc/ix/timer.h
+++ ix/inc/ix/timer.h
@@ -93,3 +93,10 @@ extern void timer_init_fg(void);
 extern int timer_init(void);
 
 extern int cycles_per_us;
+
+#ifdef ENABLE_KSTATS
+extern int timer_percpu_add(struct timer *t, uint64_t usecs);
+extern void timer_percpu_run(void);
+extern void timer_percpu_init_cpu(void);
+extern int timer_percpu_init(void);
+#endif
