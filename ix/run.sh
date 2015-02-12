7c491fec8b7b0f8ba2df4db403779b9a3012e85b
Gregory Dentinger Hill <greghill@maverick-13.Stanford.EDU>
2015-02-12 11:02:25 -0800

    start of cleaning branch

--- ix/run.sh
+++
@@ -1,1 +0,0 @@
-sudo ./ix --dev 0000:02:10.0 --cpu 0,1,2,3 -- /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 ../apps/memcached-1.4.18-ix/memcached -u nobody -m 8192
--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 aklog
 sudo aklog
 sudo insmod ./dune/dune.ko
-sudo sh -c "echo '2' > /sys/devices/pci0000\:00/0000\:00\:01.0/0000\:01\:00.0/sriov_numvfs"
-sudo ip link set p1p1 vf 0 mac e6:14:c6:39:a5:d7
-sudo ip link set p1p1 vf 0 spoofchk off
-sudo ip link set p1p1 vf 1 mac e6:14:c6:39:a5:d8
-sudo ip link set p1p1 vf 1 spoofchk off
+sudo sh -c "echo '6' > /sys/devices/pci0000\:00/0000\:00\:04.0/subsystem/devices/0000\:04\:00.0/sriov_numvfs"
+sudo ip link set p3p1 vf 0 mac e6:14:c6:39:a5:d9
+sudo ip link set p3p1 vf 0 spoofchk off
+sudo ip link set p3p1 vf 1 mac e6:14:c6:39:a5:da
+sudo ip link set p3p1 vf 1 spoofchk off
+sudo ip link set p3p1 vf 2 mac e6:14:c6:39:a5:db
+sudo ip link set p3p1 vf 2 spoofchk off
+sudo ip link set p3p1 vf 3 mac e6:14:c6:39:a5:dc
+sudo ip link set p3p1 vf 3 spoofchk off
+sudo ip link set p3p1 vf 4 mac e6:14:c6:39:a5:dd
+sudo ip link set p3p1 vf 4 spoofchk off
+sudo ip link set p3p1 vf 5 mac e6:14:c6:39:a5:df
+sudo ip link set p3p1 vf 5 spoofchk off
 #sudo rmmod ixgbevf
 #sudo rmmod ixgbe
 #sudo insmod ./igb_stub/igb_stub.ko
-sudo sh -c "echo '5000' > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
+sudo sh -c "echo '50000' > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
